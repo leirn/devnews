fe1b259257f0fec3bd8599844ffebb4dca747312
leirn <laurent@vromman.org>
2022-10-25 14:15:34 +0200

    Ajout du spport + code qui compile

--- src/borrowing/mod.rs
+++ src/borrowing/mod.rs
@@ -1,10 +1,10 @@
 #![allow(unused_variables, unused_mut)]
 
 pub fn borrowing1() {
-    let mut s:String = "こんにちは世界".into();
+    let mut s: String = "こんにちは世界".into();
     let r1 = &mut s;
-    // let r2 = &mut s; // Impossible car il y a déjà une référence mutable vers s
-    // println!("{} {}", r1, r2);
+    //let r2 = &mut s; // Impossible car il y a déjà une référence mutable vers s
+    //println!("{} {}", r1, r2);
 }
 
 pub fn borrowing2() {
@@ -29,15 +29,18 @@ pub fn borrowing4() {
     let mut s = String::from("こんにちは世界");
     let r1 = &s;
     let r2 = &s;
-    // let r3 = &mut s; // Impossible car il y a déjà des référence immmutable vers s
-    // println!("{} {} {}", r1, r2, r3);
+    //let r3 = &mut s; // Impossible car il y a déjà des référence immmutable vers s
+    //println!("{} {} {}", r1, r2, r3);
 }
 
 pub fn borrowing5() {
     let mut s = String::from("こんにちは世界");
-    let r1 = &s;
-    let r2 = &s;
-    println!("{} {}", r1, r2);
+    {
+        let r1 = &s;
+        let r2 = &s;
+        println!("{} {}", r1, r2);
+    }
     let r3 = &mut s; // Possible car r1 et r2 ne seront plus utilisée parès le println! précédent
     println!("{}", r3);
+    println!("{}", s);
 }
--- src/mutability/mod.rs
+++ src/mutability/mod.rs
@@ -1,14 +1,14 @@
-#![allow(dead_code,unused_variables)]
+#![allow(dead_code, unused_variables)]
 
 pub fn mutability1() {
-    let x = 5_u8; // x est immutable
+    let mut x = 5_u8 as u32; // x est immutable
     println!("The value of x is: {x}");
-    // x = 6; // Refusera de compiler
+    x = 6; // Refusera de compiler
     println!("The value of x is: {x}");
 }
 
 pub fn mutability2() {
-    let mut x = 5; // x est immutable
+    let mut x = 5; // x est mutable
     println!("The value of x is: {x}");
     x = 6_u16; // Compilation fonctionnelle
     println!("The value of x is: {x}");
@@ -17,7 +17,9 @@ pub fn mutability2() {
 pub fn mutability3() {
     let x = 5; // x est immutable
     println!("The value of x is: {x}");
-    let x = 6; // shadowing : fonctionne car x est redéfini
+    {
+        let x = 6; // shadowing : fonctionne car x est redéfini
+    }
     println!("The value of x is: {x}");
 }
 
